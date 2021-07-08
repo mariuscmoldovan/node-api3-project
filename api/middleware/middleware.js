46f404bd36e608e4c6626ec8a85dd31dfc918028
mariusmoldovan <biris_21@yahoo.com>
2021-07-07 22:10:07 -0500

    post update

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -17,7 +17,7 @@ async function validateUserId(req, res, next) {
       const user = await User.getById(req.params.id)
       if (!user) {
         res.status(404).json({
-          message: " no such user"
+          message: "user not found" 
         })
       }else {
         req.user = user
@@ -26,24 +26,36 @@ async function validateUserId(req, res, next) {
 
   }catch(err) {
     res.status(500).json({
-      message: " user not found"
+      message: "user not found",
     })
 
   }
-  console.log('validateUserId');
-  next()
-}
-
+ }
+// VALIDATE USER
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
-  console.log('validateUser');
-  next()
+  const { name } = req.body
+    if (!name || !name.trim()) {
+      res.status(404).json({
+        message: "missing required name field"  
+      })
+    }else {
+      req.name = name.trim()
+        next()
+    }
 }
-
+// VALIDATE POST
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-  console.log('validatePost');
-  next()
+  const { text } = req.body
+    if (!text || !text.trim()) {
+      res.status(404).json({
+        message: "missing required text field"  
+      })
+    }else {
+      req.text = text.trim()
+        next()
+    }
 }
 
 // do not forget to expose these functions to other modules
